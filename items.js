20adcd73a8fe8646f93c7a8f35ea8a147a0b83b1
Phan Huong <phanngochuong@hotmail.com>
2021-04-16 21:49:16 +0200

    Init
    Create app

---
+++ items.js
@@ -0,0 +1,21 @@
+const item1 = {
+    name: 'cheese',
+    amount: 1,
+    date: '01-05-2021'
+}
+
+const item2 = {
+    name: 'yogurt',
+    amount: 5,
+    date: '20-04-2021'
+}
+
+const item3 = {
+    name: 'salmon',
+    amount: 3,
+    date: '22-04-2021'
+}
+
+module.exports = item1;
+module.exports = item2;
+module.exports = item3;
